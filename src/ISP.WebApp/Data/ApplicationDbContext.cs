30e2c1cc1df4ad263c5ad4f0abb6997dae4f499d
Jomaroflo94 <jomaroflo94@hotmail.com>
2024-05-27 23:20:34 +0200

    Reestructuración de carpetas y modificación de nombre a IDP

--- src/ISP.WebApp/Data/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace ISP.WebApp.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
-    : IdentityDbContext<IdentityUser>(options) {}
